6136d215033cffcf77c10b0ba32596c1d67e0cb5
Hugo Sens <victorhugosens@live.com>
2023-02-20 16:17:22 -0300

    Ajustes Projeto a

--- resources/views/padrao/layout.blade.php
+++ resources/views/padrao/layout.blade.php
@@ -13,7 +13,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="{{Url('/assets_padrao/css/bootstrap.min.css')}}">
     <link rel="stylesheet" href="{{Url('/assets_padrao/bundles/izitoast/dist/css/iziToast.min.css')}}">
-    <link rel="stylesheet" href="{{Url('/assets_padrao/css/default.css')}}">
+    <link rel="stylesheet" href="{{Url('/assets_padrao/css/defaultV1.css')}}">
     @yield('css-custom')
 
 </head>
